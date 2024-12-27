dfb80f9c420510b07373a3ee8cb679d11ad246ce
dilazeynepy <dilazeynepates86@gmail.com>
2024-12-27 23:50:50 +0300

    Add files via upload

---
+++ 241227/ornek1.py
@@ -0,0 +1,4 @@
+birim_fiyat=20
+metrekup=int(input("su miktarı"))
+fatura=birim_fiyat*metrekup
+print("faturanız budur:",fatura)
---
+++ 241227/ornek2.py
@@ -0,0 +1,6 @@
+yazili1=int(input("1. sınav notu="))
+yazili2=int(input("2. sınav notu="))
+performans1=int(input("1. performans notu="))
+performans2=int(input("2. performans notu="))
+ortalama=(yazili1+yazili2+performans1+performans2)/4
+print("ders notu ortalamanız=",ortalama)
